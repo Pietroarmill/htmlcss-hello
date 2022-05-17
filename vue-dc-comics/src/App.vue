5f67c427bf700b6567f26ae64380cd10223a018a
Pietroarmill <pietroarmillotta38@gmail.com>
2022-05-17 15:31:23 +0200

    add

--- vue-dc-comics/src/App.vue
+++ vue-dc-comics/src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- Header -->
+    <header>
+      <AppHeader />
+    </header>
+    <!-- /Header -->
+    <!-- Main -->
+    <main>questo è il main</main>
+    <!-- /Main -->
+    <!-- Footer -->
+    <footer>questo è il footer</footer>
+    <!-- /Footer -->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import AppHeader from "./components/AppHeader.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    AppHeader,
   },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
---
+++ vue-dc-comics/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="container header">
+    <div class="header__image">
+      <img src="../assets/img/dc-logo.png" alt="" />
+    </div>
+    <nav>
+      <ul class="header__menu">
+        <li></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style>
+@import "../style/common.scss";
+</style>
